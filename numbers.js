3263ac582993ae7087e0476ba7d455c398fbb551
hivkosic <117200270+hivkosic@users.noreply.github.com>
2022-11-29 23:53:12 +0100

    js intro, numbers, string

---
+++ numbers.js
@@ -0,0 +1,41 @@
+
+/*var a = 1;
+var b;
+
+// Deskriptivna varijabla loverCamel case
+
+//deklaracija varijable
+var deklaracija;
+
+console.log(deklaracija);
+
+//assigment
+deklaracija = 100;
+
+console.log(deklaracija);
+console.log(typeof deklaracija);
+
+//aritmetičke operacije
+console.log(1+1)
+console.log(2-1)
+console.log(1*1)
+console.log(10/3)
+
+//modulo-ostatak pri dijeljenju
+console.log(10%3)
+
+var myString="13"
+console.log(String(myString));
+console.log(typeof String(myString));
+
+*/
+
+debugger;
+
+var i = 1;
+console.log(i);
+console.log(++i);
+console.log(i);
+console.log(i++);
+console.log(i);
+
---
+++ string.js
@@ -0,0 +1,15 @@
+var myFirstString="Hello world";
+
+var myNumber ="1"
+
+console.log(myFirstString);
+
+console.log(typeof myFirstString);
+//casting
+
+
+console.log(typeof Number(myFirstString));
+//coercion
+console.log(typeof +(myFirstString));
+
+
